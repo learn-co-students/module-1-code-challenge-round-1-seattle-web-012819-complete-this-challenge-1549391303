60390b916695e3144b2272c36abdacf5a0d0b7d4
Nat-Reid <nathanielreid@gmail.com>
2019-02-05 11:18:41 -0800

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,39 @@
 class Restaurant
+
+  @@all = []
+
   attr_accessor :name
 
-  def initialize(name)
+  def initialize(name: "Barb's Pizza Truck")
     @name = name
+    @@all << self
+  end
+
+  def reviews
+    Review.all.select{|review| review.restaurant == self}
+  end
+
+  def customers
+    self.reviews.collect{|review| review.customer}.uniq
+  end
+
+  def average_star_rating
+    ratings = self.reviews.collect{|review| review.rating}
+    ratings.reduce(:+)/ratings.count
+  end
+
+  def longest_review
+    self.reviews.max_by{|review| review.content.length}
+  end
+
+
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|r| r.name == name}
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,20 @@
 class Review
-  
-end
 
+  @@all = []
+
+  attr_reader :customer, :restaurant, :rating, :content
+
+  def initialize(restaurant: nil, customer: nil, rating: 1, content: "")
+    @customer = customer
+    @rating = [[rating,1].max,5].min
+    @content = content
+    @restaurant = restaurant
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
