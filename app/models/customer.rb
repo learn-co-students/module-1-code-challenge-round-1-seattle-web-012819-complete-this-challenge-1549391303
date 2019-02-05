7d35a5b4ebab9a2149ca3dd18c95336e077b329f
Katie Carrel <katiecarrel@comcast.net>
2019-02-05 11:27:11 -0800

    adding code challnenge files

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,76 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+
+    @@all << self
   end
 
+  #should return all of the customer instances - check
+  def self.all
+    @@all
+  end
+
+  #given a string of a full name, returns the first customer whose full name matches
+  def self.find_by_name(name)
+    name_array = name.split
+    Customer.all.each do |customer|
+      if customer.first_name == name_array[0] && customer.last_name == name_array[1]
+        return customer
+      end
+    end
+  end
+
+  #given a string of a first name, returns an array containing all customers with that first name
+  def self.find_all_by_first_name(name)
+    #seach through every instance of Customer and map a new array with instance of customer with a first name matching what is passed in
+    Customer.all.map {|customer| customer.first_name == name}
+    end
+  end
+
+  #should return an array of all the customer full names
+  def self.all_names
+    new_array = []
+    Customer.all.each do |customer|
+      new_array << customer.full_name
+    end
+    new_array
+  end
+
+  #provided method
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  #given a restaurant object, some review content (as a string), and a star rating (as an integer), creates a new review and associates it with that customer and restaurant.
+  def add_review(restaurant, content, rating)
+    Review.new(self,restaurant,rating,content)
+  end
+
+  #Returns the total number of reviews that a customer has authored
+  def num_reviews
+    count = 0
+    Review.all.select do |review|
+      if review.customer == self
+        count += 1
+      end
+    end
+    return count
+  end
+
+  #Returns a unique array of all restaurants a customer has reviewed
+  def restaurant
+    new_array = []
+    Review.all.select do |review|
+      if review.customer == self
+        new_array << review.restaurant
+      end
+    end
+    return new_array
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,85 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
   def initialize(name)
     @name = name
+
+    @@all << self
+  end
+
+  #returns an array of all restaurants - check
+  def self.all
+    @@all
+  end
+
+  #given a string of restaurant name, returns the first restaurant that matches
+  def self.find_by_name(name)
+    Restaurant.all.find do |restaurant|
+      if restaurant.name == name
+        return restaurant
+      end
+    end
+  end
+
+  #Returns a unique list of all customers who have reviewed a particular restaurant.
+
+  #expect that they will need to specify the particular restaurant and check all reviews against
+  def customers(name)
+    all_reviewers = []
+    Review.all.each do |review|
+      if review.restaurant == name
+        all_reviewers << review.customer
+      end
+    end
+    return all_reviewers
+  end
+
+  #returns an array of all reviews for that restaurant
+  def reviews
+    all_reviews = []
+    Review.all.each do |review|
+      if review.restaurant == self
+        all_reviews << review
+      end
+    end
+    all_reviews
+  end
+
+  #returns the average star rating for a restaurant based on its reviews
+  def average_star_rating
+    all_reviews = []
+    total = 0
+    Review.all.each do |review|
+      if review.restaurant == self
+          total += review.rating
+          all_reviews << review
+      end
+    end
+    number = all_reviews.length
+    return total.to_f/number
+  end
+
+  #returns the longest review content for a given restaurant
+
+  #check all reviews  for reviews for restaurant
+  #create a tally to keep track of the length of each restaurant review
+  #return longest at the end
+
+  def longest_review
+    tally = Hash.new(0)
+    longest_length = 0
+    longest_review = nil
+    Review.all.each do |review|
+      if review.restaurant == self
+        tally[review.length] += review.content.length
+        if tally[review.length] > longest_length
+          longest_length = tally[review.length]
+          longest_review = review.content
+        end
+      end
+    end 
+    return longest_review
   end
 
 end
