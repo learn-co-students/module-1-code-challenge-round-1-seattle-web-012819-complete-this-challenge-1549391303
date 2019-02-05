697221ec4304c275e20f124f2f523daf87a71c5f
kwmcminn <kwmcminn@gmail.com>
2019-02-05 11:34:32 -0800

    Kevin's first code challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,36 @@
 class Review
-  
-end
+  @@all = []
+  attr_accessor :rate, :review
+  attr_reader :name, :full_name, :date
 
+
+  def initialize(full_name, name, date, rate, review)
+    @name = name
+    @full_name = full_name
+    @date = date
+    @rate = rate
+    @review = review
+
+    @@all << self
+  end
+
+  def customer
+    self.full_name
+  end
+
+  def restaurant
+    self.name
+  end
+
+  def rating
+    self.rate
+  end
+
+  def content
+    self.review
+  end
+
+  def self.all
+    @@all
+  end
+end
