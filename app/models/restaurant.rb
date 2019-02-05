fa1b206d11e065ab05249637e1f17eba9f20651d
jasonbasuil <basuilj@gmail.com>
2019-02-05 11:35:02 -0800

    Done.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,48 @@
 class Restaurant
+
+  @@all = []
+
   attr_accessor :name
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    #given a string of restaurant name, returns the first restaurant that matches
+  end
+
+  def customers
+    #Returns a unique list of all customers who have reviewed a particular restaurant.
+    Review.all.select do |customer|
+      customer.restaurant.uniq
+    end
+  end
+
+  def reviews
+    Review.all.map do |review|
+      review.content == self
+    end
+  end
+
+  def average_star_rating
+    Review.all.map do |rating|
+      rating.rating == self
+    end
+  end
+
+  def longest_review
+    longest = nil
+    Review.all.each do |review|
+      if review.content.length > longest
+        longest << review
+      end
+    end 
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,11 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+jason = Customer.new("Jason", "Basuil")
+madeline = Customer.new("Madeline", "Dixon")
+
+chilis = Restaurant.new("Chilis")
+tacobell = Restaurant.new("Taco Bell")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
