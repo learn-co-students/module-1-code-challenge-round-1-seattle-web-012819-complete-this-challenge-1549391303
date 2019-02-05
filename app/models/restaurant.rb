910d7699da8d10e9d32090aa51e005b704707a1d
github username <github email address>
2019-02-05 11:26:48 -0800

    YAY DONE

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,68 @@
+require "pry"
+
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |restaurant|
+      restaurant.name.downcase == name.downcase
+    end
+  end
+
+  def customers
+    arr = Review.all.select do |rev|
+      rev.restaurant == self
+    end
+    arr2 = arr.collect do |rev|
+      rev.customer
+    end
+    arr2.uniq
+  end
+
+  def reviews
+    Review.all.select do |rev|
+      rev.restaurant == self
+    end
+  end
+
+  def average_star_rating
+    total = 0
+
+    arr = Review.all.select do |rev|
+      rev.restaurant == self
+    end
+
+    count = arr.size
+
+    arr.each do |element|
+      total += element.rating
+    end
+
+    total/count
+  end
+
+  def longest_review
+    arr = Review.all.select do |rev|
+      rev.restaurant == self
+    end
+
+    longest_rev = arr[0]
+
+    arr.each do |element|
+      if element.content.length > longest_rev.content.length
+        longest_rev = element
+      end
+    end
+    longest_rev
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,39 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+bob = Customer.new("Bob", "Thorton")
+emily = Customer.new("Emily", "Wagner")
+sally = Customer.new("Sally", "Smith")
+billy = Customer.new("Billy", "Bob")
+josh = Customer.new("Josh", "Weldon")
+tom = Customer.new("Tom", "Scott")
+emilyr = Customer.new("Emily", "Reagan")
+emilyt = Customer.new("Emily", "Travis")
+
+fns = Restaurant.new("Food n Stuff")
+mfs = Restaurant.new("More Food Store")
+
+bad_review = Review.new(fns, emily, 1, "Really really bad")
+meh_review = Review.new(mfs, sally, 3, "Kind of meh")
+good_review = Review.new(fns, tom, 5, "Very Good")
+long_review = Review.new(fns, tom, 5, "This is a really really really really long review")
+
+
+#----------Guest Class Tests
+# puts Customer.all
+# puts Customer.find_by_name("Emily Wagner").first_name
+# puts Customer.find_all_by_first_name("Emily")
+# puts Customer.all_names
+
+#----------Restaurant Class Tests
+# puts Restaurant.all
+# puts Restaurant.find_by_name("Food n Stuff").name
+
+#----------Review Class Tests
+# puts Review.all
+
+puts emily.add_review(mfs, "ok enough", 3)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
