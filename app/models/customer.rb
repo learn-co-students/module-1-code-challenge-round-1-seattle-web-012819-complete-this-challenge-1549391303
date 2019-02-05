8a0bdfa484a810f2ff4461f4ace6f6f23a66412c
github username <github email address>
2019-02-05 11:21:16 -0800

    Finished

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,78 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  # returns list of all Customer instances
+  # Customer.all -> [Customer: instances]
+  def self.all
+    @@all
+  end
+
+  # given full name String, returns first matching Customer
+  # Customer.find_by_name(String) -> Customers
+  def self.find_by_name(full_name)
+    customers = self.all.select do |customer|
+      customer.full_name == full_name
+    end
+    customers.first
+  end
+
+  # given first name String, returns array of matching Customers
+  # Customer.find_all_by_first_name(String) -> [Customers]
+  def self.find_all_by_first_name(first_name)
+    self.all.select do |customer|
+      customer.first_name == first_name
+    end
+  end
+
+  # returns array of all Customer full names
+  # Customer.all_names -> [String]
+  def self.all_names
+    self.all.map do |customer|
+      customer.full_name
+    end
+  end
+
+  # given Restaurant instance, review as a String, and rating as Integer from 1 - 5
+  # create and return Review instance
+  # #add_review(Restaurant: instance, content: String, rating (1 - 5): Integer)
+  def add_review(restaurant, content, rating)
+    Review.new(restaurant, self, content, rating)
+  end
+
+  # returns list of Reviews Customer instance has made
+  # #reviews -> [Review: instance]
+  def reviews
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  # returns number of Reviews Customer instance has made
+  # #num_reviews -> Integer
+  def num_reviews
+    self.reviews.length
+  end
+
+  # returns list of Restaurant instances Customer has reviewed
+  # #restaurants -> [Restaurant: instances]
+  def restaurants
+    reviewed_restaurants = self.reviews.map do |review|
+      review.restaurant
+    end
+    reviewed_restaurants.uniq
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,42 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
 
-binding.pry
+me = Customer.new('Ezra', 'Schwepker')
+toby = Customer.new('Toby', 'McGuire')
+elon = Customer.new('Elon', 'Musk')
+
+puts elon.full_name
+puts Customer.all
+puts Customer.all_names
+puts Customer.find_by_name(elon.full_name)
+puts Customer.find_all_by_first_name(elon.first_name)
+
+westward = Restaurant.new('Westward')
+mcdonalds = Restaurant.new('McDonalds')
+panisse = Restaurant.new('Chez Panisse')
+
+me.add_review(panisse, "this is pretty good", 4)
+me.add_review(mcdonalds, "best meal of my life", 5)
+me.add_review(westward, "meh", 3)
+me.add_review(westward, "actually that was ok", 4)
+
+toby.add_review(mcdonalds, "ehhhhhh", 1)
+elon.add_review(mcdonalds, "mehmeh", 2)
+
+puts me.reviews
+puts me.num_reviews
+puts me.restaurants
+
+puts Restaurant.all
+puts Restaurant.find_by_name(westward.name)
+
+puts westward.reviews
+puts westward.customers
+puts westward.average_star_rating
+puts westward.longest_review
+
+puts Review.all
+
+# binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
