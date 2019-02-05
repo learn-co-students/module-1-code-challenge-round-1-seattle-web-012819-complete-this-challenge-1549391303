9f168210575260ae4b2d33f3982f9b53e46dae41
github username <github email address>
2019-02-05 11:24:45 -0800

    Completed the first module test early.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,42 @@
 class Customer
+  @@all = []
   attr_accessor :first_name, :last_name
-
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.find_by_name(name)
+    self.all.select { |c| c.first_name == name.split(" ")[0] && c.last_name == name.split(" ")[1] }.first
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select { |c| c.first_name == name}
+  end
+
+  def self.all_names
+    self.all.map { |c| c.first_name+c.last_name }
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, content, rating)
+  end
+
+  def num_reviews
+    n = Review.all.select { |rv| rv.customer == self }.length
+    !n ? 0 : n
+  end
+
+  def restaurants
+    Review.all.select { |rv| rv.customer == self }.map { |rv| rv.restaurant }.uniq
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,25 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+## Customers
+# men
+bob_hope = Customer.new("Bob", "Hope")
+john_smith = Customer.new("John", "Smith")
+frank_sinatra = Customer.new("Frank", "Sinatra")
+# women
+sally_fields = Customer.new("Sally", "Fields")
+Jane_fonda = Customer.new("Jane", "Fonda")
+marilyn_monroe = Customer.new("Marilyn", "Monroe")
+
+## Restaurants
+solstice_cafe = Restaurant.new("Solstice Cafe")
+grateful_bread = Restaurant.new("Grateful Bread")
+la_marzoccco = Restaurant.new("La Marzocco")
+
+## Ratings
+bob_hope.add_review(solstice_cafe, "Great little cafe.", 3)
+john_smith.add_review(solstice_cafe, "Hipsters.", 2)
+frank_sinatra.add_review(solstice_cafe, "Great place to eat and I can't wait to go back!", 5)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
